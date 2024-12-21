a912026cb35e9058945423df87e847902bde3a52
Yashu Tanna <6696871+yashutanna@users.noreply.github.com>
2024-12-21 12:42:26 +0200

    add staking and DeFi rates

--- src/valr/metrics.ts
+++ src/valr/metrics.ts
@@ -1,10 +1,12 @@
 import MetricService from "../api/metrics/service";
-import Valr from "./valr";
+import Valr, {VALR_EARN_TYPES} from "./valr";
 import BigNumber from "bignumber.js";
 
 export enum GAUGE_NAMES {
     valr_upcoming_lend_rate = 'valr_upcoming_lend_rate',
     valr_lend_rate = 'valr_lend_rate',
+    valr_defi_lend_rate = 'valr_defi_lend_rate',
+    valr_stake_rate = 'valr_stake_rate',
     valr_eth_price = 'valr_eth_price',
 }
 
@@ -23,6 +25,11 @@ export class ValrMetrics {
                 name: GAUGE_NAMES.valr_upcoming_lend_rate,
                 help: "Upcoming estimated Lend APY at VALR",
                 labelNames: ["coin"],
+            },
+            {
+                name: GAUGE_NAMES.valr_stake_rate,
+                help: "Upcoming estimated Stake APY at VALR",
+                labelNames: ["coin"],
             }
         ];
         nonCollectionGauge.map(gauge => {
@@ -35,6 +42,13 @@ export class ValrMetrics {
                 labelNames: ["coin"],
                 collect: this.getLendAPYs.bind(this),
                 collectIntervalMs: process.env.VALR_LEND_RATE_METRIC_INTERVAL ? parseInt(process.env.VALR_LEND_RATE_METRIC_INTERVAL) : 10000
+            },
+            {
+                name: GAUGE_NAMES.valr_defi_lend_rate,
+                help: " Defi  Lend APY earned at VALR by the bot",
+                labelNames: ["coin"],
+                collect: this.getStakingRates.bind(this),
+                collectIntervalMs: process.env.VALR_DEFI_LEND_RATE_METRIC_INTERVAL ? parseInt(process.env.VALR_DEFI_LEND_RATE_METRIC_INTERVAL) : 10000
             },
             {
                 name: GAUGE_NAMES.valr_eth_price,
@@ -74,6 +88,28 @@ export class ValrMetrics {
             }))
         } catch (e) {
             console.error("could not update borrow metrics", e)
+        }
+    }
+    async getStakingRates(): Promise<void> {
+        try {
+            const lendingRates = await this.valr.getStakingRates(VALR_EARN_TYPES.LEND);
+            await Promise.all(lendingRates.map(async ({currencySymbol, rate}) => {
+                this.metricsService.updateGauge(
+                    GAUGE_NAMES.valr_defi_lend_rate,
+                    Number(new BigNumber(rate).multipliedBy(24).multipliedBy(365)),
+                    { coin: currencySymbol.toLowerCase() }
+                )
+            }))
+            const stakingRates = await this.valr.getStakingRates(VALR_EARN_TYPES.STAKE);
+            await Promise.all(stakingRates.map(async ({currencySymbol, rate}) => {
+                this.metricsService.updateGauge(
+                    GAUGE_NAMES.valr_stake_rate,
+                    Number(new BigNumber(rate).multipliedBy(24).multipliedBy(365)),
+                    { coin: currencySymbol.toLowerCase() }
+                )
+            }))
+        } catch (e) {
+            console.error(`could not staking metrics for`, e)
         }
     }
     async getValrEthPrice(): Promise<void> {
--- src/valr/valr.ts
+++ src/valr/valr.ts
@@ -6,6 +6,13 @@ export type ValrLendRates = {
     estimatedNextRate: number,
     estimatedNextBorrowRate: string
 };
+export type ValrStakeRates = {
+    currencySymbol:string,
+    rate: string,
+    periodIntervalMinutes: number,
+    minimumStakeAmount: string,
+    earnType: 'Lend' | 'Stake'
+};
 export type MarketSummary = {
     "currencyPair": string;
     "askPrice": string;
@@ -21,6 +28,11 @@ export type MarketSummary = {
     "markPrice": string;
 };
 
+export enum VALR_EARN_TYPES {
+    LEND= 'LEND',
+    STAKE= 'STAKE',
+}
+
 export default class Valr {
     private readonly apiKey: string;
     private readonly apiSecret: string;
@@ -33,6 +45,17 @@ export default class Valr {
     async getLendingRates(): Promise<ValrLendRates[]> {
         let timestamp = (new Date()).getTime();
         const path = `/v1/loans/rates`
+        const options = {
+            method: "GET",
+            headers: getHeaders(this.apiKey, this.apiSecret, timestamp, "GET", path)
+        };
+        const response = await fetch(`https://api.valr.com${path}`, options);
+        const json = await response.json();
+        return json
+    }
+    async getStakingRates(earnType: VALR_EARN_TYPES): Promise<ValrStakeRates[]> {
+        let timestamp = (new Date()).getTime();
+        const path = `/v1/staking/rates?earnType=${earnType}`
         const options = {
             method: "GET",
             headers: getHeaders(this.apiKey, this.apiSecret, timestamp, "GET", path)
